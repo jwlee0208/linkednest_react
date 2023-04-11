c80544e9282809edb7932167d9bc0bde5167b59b
LEE JINWON <jwlee0208@gmail.com>
2023-04-11 14:56:26 +0900

    2023.04.11 commit

--- src/main/java/net/linkednest/backoffice/repository/AdminRoleRepository.java
+++
@@ -1,7 +0,0 @@
-package net.linkednest.backoffice.repository;
-
-import net.linkednest.common.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRoleRepository extends JpaRepository<Role, Long> {
-}
