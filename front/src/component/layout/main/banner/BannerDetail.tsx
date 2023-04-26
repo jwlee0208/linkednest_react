8b1761cb960596bafe5c8590e47e475ce004ba0f
LEE JINWON <jwlee0208@gmail.com>
2023-04-26 10:16:53 +0900

    2023.04.26 commit - home > 최근 공지 component 수정

--- front/src/component/layout/main/banner/BannerDetail.tsx
+++ front/src/component/layout/main/banner/BannerDetail.tsx
@@ -9,7 +9,7 @@ type BannerProps = {
 function BannerDetail({info} : BannerProps) {
     return (
         <Paper title={info.bannerName}>
-            <Image src={info.mainImageUrl} style={{margin:"auto", display:"flex"}} alt={info.bannerDesc}/>
+            <Image src={info.mainImageUrl} style={{margin:"auto", display:"flex", maxHeight:"400px"}} alt={info.bannerDesc}/>
         </Paper>
     )
 }
