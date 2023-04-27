fe748166678e58f405c4072a168fec2daec6061d
LEE JINWON <jwlee0208@gmail.com>
2023-04-27 17:27:58 +0900

    2023.04.27 commit - portal 및 component 일부 css 수정

--- front/src/component/layout/common/headerArea/PortalHeader.tsx
+++ front/src/component/layout/common/headerArea/PortalHeader.tsx
@@ -18,7 +18,10 @@ import { styled, useTheme } from '@mui/material/styles';
 import Image from 'mui-image';
 import * as React from 'react';
 import { ContentList_ } from '../../../../store/modules/content';
-import { User } from '../../../../store/modules/user';
+import { User, asyncLogout } from '../../../../store/modules/user';
+import { Avatar, Button, ButtonGroup, Menu, MenuItem, Tooltip } from '@mui/material';
+import { useNavigate } from 'react-router';
+import { useAppDispatch } from '../../../../store/index.hooks';
 
 
 type PortalMenuProps = {
@@ -108,11 +111,54 @@ function PortalHeader({
     window.location.href = `${url}`;
   }
 
+  const settings = [{menu : 'My Page', path : `/${contentCode}/mypage`}, {menu : 'Logout', path : '/logout'}];
+  const pages    = (isLogin === true) ? [{menu : 'Home', path : `/${contentCode}`}
+                                       , {menu : 'My Page', path : `/${contentCode}/mypage`}] 
+                                      : [{menu : 'Home', path : `/${contentCode}`}];
+
+  const navigate    = useNavigate();
+  const dispatch    = useAppDispatch();
+  
+  const [anchorElNav, setAnchorElNav]   = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+
+  const handleOpenNavMenu   = (event: React.MouseEvent<HTMLElement>) => {
+      setAnchorElNav(event.currentTarget);
+  };
+  
+  const handleOpenUserMenu  = (event: React.MouseEvent<HTMLElement>) => {
+      setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu  = (param : string, event : React.MouseEvent<HTMLElement>) => {
+    navigate(param);
+    setAnchorElNav(null);
+  };
+
+  const handleCloseNavMenu_ = () => {
+    setAnchorElNav(null);
+  }
+
+  const handleCloseUserMenu = (param : string, event : React.MouseEvent) => {
+      if (param === '/logout') {
+        dispatch(asyncLogout());
+        navigate(`/${contentCode}`);    
+      } else {
+        navigate(param);
+      }
+      setAnchorElUser(null);
+  };
+
+  const handleCloseUserMenu_ = () => {
+      setAnchorElUser(null);
+  };
+
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar position="fixed" open={open} sx={{backgroundColor:'#330000'}}>
-        <Toolbar>
+        <Toolbar sx={{width:'100%'}}>
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -122,9 +168,45 @@ function PortalHeader({
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap component="div">
+          <Typography variant="h6" noWrap component="div" sx={{width:'20%', }}>
             Test Project
           </Typography>
+          {(isLogin === true) ? (
+            <Box sx={{ width:'100%', flexGrow: 0 }}>                
+              <Tooltip title="Open settings" sx={{float:'right'}}>
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0, float:'right' }}>
+                  <Avatar alt="Remy Sharp" src="images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu sx={{ mt: '45px' }} 
+                    id="menu-appbar" 
+                    anchorEl={anchorElUser} 
+                    anchorOrigin={{vertical: 'top', horizontal: 'right',}} 
+                    keepMounted 
+                    transformOrigin={{vertical: 'top', horizontal: 'right',}} 
+                    open={Boolean(anchorElUser)} 
+                    onClose={handleCloseUserMenu_}>
+                {settings.map((setting) => (
+                  <MenuItem key={setting.menu} 
+                            onClick={(e)=>{handleCloseUserMenu(setting.path, e)}}>
+                    <Typography textAlign="center">{setting.menu}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>              
+    ) : (
+        <Box sx={{ width:'100%', flexGrow: 0 }}>        
+          <ButtonGroup sx={{float:'right'}}>
+            <Button onClick={(e)=>{handleCloseNavMenu(`/${contentCode}/signup`, e)}} sx={{ my: 2, color: 'white', display: 'block' }} variant='contained'>SignUp</Button>  
+            {
+              layoutType === '3' || layoutType === '0' ? (
+                <Button onClick={(e)=>{handleCloseNavMenu(`/${contentCode}/login`, e)}} sx={{ my: 2, color: 'white', display: 'block' }} variant='outlined'>SignIn</Button>          
+              ) : (<></>)
+            }
+          </ButtonGroup>        
+        </Box>      
+    )
+  }          
         </Toolbar>
       </AppBar>
       <Drawer
