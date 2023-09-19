6620bbfd72b19eb8cd3fa646cdf51b2909c0375f
LEE JINWON <jwlee0208@gmail.com>
2023-09-19 11:28:21 +0900

    Update JwtAuthenticationFilter.java

--- src/main/java/net/linkednest/common/filter/JwtAuthenticationFilter.java
+++ src/main/java/net/linkednest/common/filter/JwtAuthenticationFilter.java
@@ -24,7 +24,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     public JwtAuthenticationFilter(JwtProvider jwtProvider) {
         this.jwtProvider = jwtProvider;
     }
-
+/*
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         log.info("[{}.{}] request uri : {}", this.getClass().getName(), "doFilterInternal", request.getRequestURI());
@@ -50,5 +50,40 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         filterChain.doFilter(request, response);
     }
+*/
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+        log.info("[{}.{}] request uri : {}", this.getClass().getName(), "doFilterInternal", request.getRequestURI());
+        boolean isNotNeedJwt = (request.getMethod().equals(HttpMethod.OPTIONS.name()) || request.getRequestURI().contains("/images") || request.getRequestURI().contains("/images") || request.getRequestURI().contains("/style") || request.getRequestURI().contains("/reIssueToken"));
+        if (isNotNeedJwt) {
+            // Http Method : OPTION은 PASS
+            log.info("[{}.{}] HTTP METHOD OPTION REQUEST >> ", this.getClass().getName(), "doFilterInternal");
+        } else {
+            String token = jwtProvider.resolveToken(request);
+            boolean isValidToken = (token != null && jwtProvider.validateToken(token));
+            log.info("[{}.{}] isValidToken : {}", this.getClass().getName(), "doFilterInternal", isValidToken);
+            if (isValidToken) {
+                log.info("[{}.{}] token : {}", this.getClass().getName(), "doFilterInternal", token);
+                // accessToken 확인
+                token = token.split(" ")[1].trim();
+                log.info("[{}.{}] accessToken 확인 : {}", this.getClass().getName(), "doFilterInternal", token);
+                Authentication authentication = jwtProvider.getAuthentication(token);
+                log.info("[{}.{}] isSecuriuAuthenticated : {}", this.getClass().getName(), "doFilterInternal", authentication.isAuthenticated());
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            log.info("[{}.{}] response header : {}", this.getClass().getName(), "doFilterInternal", response.getHeader("REFRESH_TOKEN"));
+        }
+        try {
+            filterChain.doFilter(request, response);
+        } catch (IOException e) {
+            log.error("[{}.{}] IOException MEssages : {}", this.getClass().getName(), "doFilterInternal", e.getMessage());
+            throw new RuntimeException(e);
+        } catch (ServletException e) {
+            log.error("[{}.{}] ServletException MEssages : {}", this.getClass().getName(), "doFilterInternal", e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+    
 }
 
